a771663a0e5954f6c650556db925a0dd6bd3c0d3
Jennifer Bonapace <jennifer.bonapace@gmail.com>
2017-02-27 16:31:47 -0500

    at 4.30

--- factory2.js
+++ factory2.js
@@ -1,19 +1,22 @@
 var app = angular.module("myMod");
 
 app.factory('apiFactory', function($http){
+  var __name of object__ ={};
 
   return{
     grabData: function(){
-      return $http({
-        url:"https://ronreiter-meme-generator.p.mashape.com/meme",
-        method:'GET'  
-      })
+     $http({
+        url:"https://yoda.p.mashape.com/yoda",)}
+        method:'GET'
+        headers:{
+          "X-Mashape-Key":"cO8KAT9sA1mshhfncngbC5GjFi5qp1cYs7jjsn8f7O1ti8UH6H",
+           "Accept": "text/plain"
+         }
+       },
       .then(function success(response){
         return response;
       },  function(error) {
         console.log(error);
       });
-    }
-  }
-
+//here i have no clue what we are doing?
 });
